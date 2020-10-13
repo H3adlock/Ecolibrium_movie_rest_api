2adc60fa357b19a2660ae9c215c954b1ac0c9b44
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-10-13 20:58:24 +0530

    api for adding movies fixed

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('movies', MovieListView.as_view(), name='api')
+    path('movies', MovieListView.as_view(), name='api'),
+    path('movies/create', MovieCreateView.as_view(), name='api_create')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from rest_framework import status
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import permission_classes
@@ -7,6 +8,7 @@ from rest_framework.decorators import permission_classes
 from .models import Movie
 from .serializers import MovieSerializer
 from .permissions import IsAdminOrReadOnly
+
 
 # Create your views here.
 class MovieListView(APIView):
@@ -17,7 +19,7 @@ class MovieListView(APIView):
 
     def get(self, request, *args, **kwargs):
         queryset = Movie.objects.all()
-
+        allowed_methods = ['GET']
         name = request.query_params.get('name', None)
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
@@ -27,22 +29,23 @@ class MovieListView(APIView):
             queryset = queryset.filter(director__icontains=director)
 
         # filter if movie has genre with queried genre name
-        genre = request.query_params.get('genre', None)
-        if genre is not None:
-            queryset = queryset.filter(genre__name__icontains=genre)
+        # genre = request.query_params.get('genre', None)
+        # if genre is not None:
+        #     queryset = queryset.filter(genre__name__icontains=genre)
 
-        director = request.query_params.get('director', None)
-        if director is not None:
-            queryset = queryset.filter(director__icontains=director)
-
-        # TODO : integer filter for popularity and imdb score
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @permission_classes([IsAdminOrReadOnly])
-    def post(self, request, format=None):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # @permission_classes([IsAdminOrReadOnly])
+    # def post(self, request, format=None):
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieCreateView(generics.CreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = (IsAdminOrReadOnly,)
