44681da0d27efdb900701edfcb86297473b4d5d5
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-10-14 00:56:19 +0530

    heroku prod steps

--- ecolibrium_movie/settings/prod.py
+++ ecolibrium_movie/settings/prod.py
@@ -49,8 +49,8 @@ LOGGING = {
     }
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
