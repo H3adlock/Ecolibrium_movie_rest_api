dd1a1eef914e82d795ba96775a9c11d4ecdb0f12
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-10-14 06:37:25 +0530

    heroku prod steps

--- ecolibrium_movie/settings/prod.py
+++ ecolibrium_movie/settings/prod.py
@@ -58,3 +58,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'live-static-files', 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# s3 storage configurations
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+AWS_QUERYSTRING_AUTH = False
