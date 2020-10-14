82cafcf64fb57272df6de8aa229170befc036b04
Sourav Ghosh <sourav.ghosh700@hotmail.com>
2020-10-14 07:26:39 +0530

    code cleanup and production fixed

--- ecolibrium_movie/urls.py
+++ ecolibrium_movie/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api-token-auth/', obtain_auth_token),
                   path('api-auth/', include('rest_framework.urls')),
-                  path('', include(movie_url))
+                  path('api', include(movie_url))
               ]
 if settings.DEBUG:
     import debug_toolbar
